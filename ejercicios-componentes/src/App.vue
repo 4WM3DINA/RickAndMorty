45592438a78dae87b9accd7a80ab79b4f122db96
Ambar Medina <ambmedina8@gmail.com>
2022-06-29 19:54:44 -0400

    first commit

---
+++ ejercicios-componentes/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h1>Rick And Morty</h1>
+    <ComponentePersonajes
+      v-for="(character, index) in characters"
+      :key="index"
+      :datos="character"
+    />
+  </div>
+</template>
+
+<script>
+import ComponentePersonajes from "./components/ComponentePersonajes.vue";
+
+export default {
+  name: "App",
+  components: {
+    ComponentePersonajes,
+  },
+  data() {
+    return {
+      characters: [
+        {
+          nombre: "Rick Sanchez",
+          src: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+        },
+        {
+          nombre: "Morty Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+        },
+        {
+          nombre: "Summer Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+        },
+        {
+          nombre: "Beth Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+        },
+        {
+          nombre: "Jerry Smith",
+          src: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+        },
+        {
+          nombre: "Abadango Cluster Princess",
+          src: "https://rickandmortyapi.com/api/character/avatar/6.jpeg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  background-image: url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 2rem;
+  border: 20px solid rgb(119, 23, 23);
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  font-weight: bold;
+  font-size: 3rem;
+  color: #eb8996;
+}
+</style>
---
+++ ejercicios-componentes/src/components/ComponentePersonajes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="personajes">
+    <img :src="datos.src" :alt="datos.nombre" class="imagen" />
+    <p>{{ datos.nombre }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComponentePersonajes",
+  props: {
+    datos: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.personajes {
+  display: flex;
+}
+.imagen {
+  border-radius: 50%;
+  margin-left: 35%;
+  margin-bottom: 20px;
+  border: 5px solid rgb(21, 180, 21);
+}
+</style>
